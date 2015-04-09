f5b4c87cad585e69f37667255dee333ae879da40
dvsanthosh@gmail.com <dvsanthosh@gmail.com>
2015-04-09 17:53:30 -0400

    latlng to address conversion

--- LatLngtoAddress/src/com/eds/ReverseGooleGeoCode.java
+++ LatLngtoAddress/src/com/eds/ReverseGooleGeoCode.java
@@ -1,4 +1,7 @@
 /**
+ * This project is part of identifying Locations for Pickup and dropoff locations from latlng in the NYC Taxi data.
+ * 
+ * 
  * Use Google's GeoCode Api to Autocomplete a partital address provided 
  * Google's Geocode library
  * https://developers.google.com/maps/documentation/geocoding/    
@@ -7,16 +10,21 @@
  * http://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway
  * Input Partial Address
  * OutPut List of possible Address Match
+ * 
  */
-package com.addtech;
+ 
+package com.eds;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Scanner;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,26 +34,61 @@ import org.json.JSONObject;
  * @author D.V.Santhosh
  *
  */
-public class fetch{
+public class ReverseGooleGeoCode {
 
 	/**
-	 * Prints all the postal addresses for a user query 
-	 * 
-	 * 
+	 * @param args
+	 * @throws IOException 
+	 * @throws JSONException 
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) throws JSONException {
+	public static void main(String[] args) throws IOException, JSONException, InterruptedException {
 		
+		//API Specs
 		String URLString = "http://maps.googleapis.com/maps/api/geocode/json?"; //use "address=" or latlng=<lat>,<lang>;
 		
-		String address = getUserInput();								//assert address = "1602+Amphitheatre+Parkway"
-		String results = fetchSuggestions(URLString, address);		// This has fetched entire reply from Google GeoCode API
+		// Input file
+		File inputFile = new File("C:\\Users\\D.V.Santhosh\\Desktop\\EDS\\First2500LatLng.csv");
 		
+		//Results file
+		String absolutePath = inputFile.getAbsolutePath();
+		String path = absolutePath.substring(0,absolutePath.lastIndexOf(File.separatorChar));
+		String outputFile = path+"\\Results.csv";
+		BufferedWriter writer = null;
+		try{
+			writer = new BufferedWriter(new FileWriter(new File(outputFile)));
+		}catch (Exception e){
+			e.printStackTrace();
+			}
 		
-		// parse the Result String to JSON
-        JSONObject myJSONResult = new JSONObject(results);
-  	   	for (int i = 0; i <((JSONArray) myJSONResult.get("results")).length(); i++)
-		System.out.println(((JSONObject) ((JSONArray) myJSONResult.get("results")).get(i)).get("formatted_address"));
+		//Read input file
+
+		BufferedReader bufReader = new BufferedReader(new FileReader(inputFile));
+		String line= bufReader.readLine();				// first line has got column header
+		while((line = bufReader.readLine()) != null)
+		{
+			String [] lnglat = line.split(",");
+			String latlng = "latlng="+lnglat[1]+","+lnglat[0]; 				//"+"&result_type=street_address&key=API_KEY" can be used for restricted result but needs https with API_KEY;
+			Thread.sleep(200);
+			String results = fetchSuggestions(URLString, latlng);
+
 		
+			// parse the Result String to JSON
+			JSONObject myJSONResult = new JSONObject(results);
+			String address = null;
+			try{address = (String) ((JSONObject) ((JSONArray) myJSONResult.get("results")).get(0)).get("formatted_address");}catch(Exception e){}
+
+
+			
+			
+			// Writing output to a file result
+			try {	
+				System.out.println(line +"," + address);
+			    writer.write(line+","+address+"\n");
+
+			} catch (IOException ex) {}
+		}
+		bufReader.close();
 	}
 	
 	/**
@@ -79,17 +122,4 @@ public class fetch{
 		return replyString;
 	}
 	
-	/**
-	 * This gets address as user Input and formats as query tokens
-	 * 
-	 */
-
-	private static String getUserInput() {
-		System.out.println(" Enter few chars from Address");
-		Scanner in = new Scanner(System.in);
-		String address = "address=" + (in.nextLine().replace(" ", "+"));
-		in.close();
-		return address;
-	}
-
 }
